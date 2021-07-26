9406b6f7c1a69225e4cf65f88826abc5f6a28140
abel <abelmurgasdev@gmail.com>
2021-07-26 11:47:03 -0500

    Se termino el proyecto en general

--- Interrupciones_multiples.py
+++ Interrupciones_multiples.py
@@ -5,6 +5,7 @@ class Interucciones_multiple():
         self.proceso = [[0,"programa",10,False]]
         self.Clasificacion_procesos_list = []
         self.time = 0
+        self.cola = []
     def Agregar_Dispositvo(self,Dispositivo,Prioridad): 
         """Agrega los dispositivon en conjunto con su prioridad y los coloca en eu diccionario"""
         Dispositivo = Dispositivo.lower()
@@ -14,6 +15,14 @@ class Interucciones_multiple():
         except:
             return False
 
+    def cola_pendiente(self,nombre,tiempo_faltante):
+        self.cola.append([nombre,tiempo_faltante])
+        
+    def Dispositivo_igual(self,dispositivo):
+        for i in self.Dispositivo_Prioridad:
+            if i == dispositivo:
+                return True
+        return False
 
     def Agrergar_proceso(self,Tiempo_inicial_Proceso,Dispositivo,Duracion):
         """Optiene los 3 datos: El tiempo en el que inicia, el dispositivo que corresponde y la duracion del proceso, y los coloca en una lista dentro de la Lista *Procesos*"""
@@ -48,7 +57,7 @@ class Interucciones_multiple():
         except:False
 
     def Punto_Proceso(self,aux,time):
-        """dirije el auxiliar al punto en el proceso que debe ser desarrollado, dependiendo de tanto jerarquia(quien tiene mas prioridad) como si es el unico proceso pendiente, este retornara el auxialiar que dirige el proximo proxeso a ejecutarse"""
+        """dirije el auxiliar al punto en el proceso que debe ser desarrollado, dependiendo de tanto jerarquia(quien tiene mas prioridad) como si es el unico proceso pendiente, este retornara el auxialiar que dirige el proximo proceso a ejecutarse"""
 
         if (len(self.proceso)) <= aux: #Si el auxiliar llega a medir mas que la longitud
             aux = len(self.proceso)-1 #Se coloca el auxiliar al punto maximo de la lista
@@ -89,11 +98,13 @@ class Interucciones_multiple():
                     aux = aux - 1
                 if self.Interruccion_proceso(self.time, aux): 
                     if self.Jerarquia_ASC(aux):
-                        self.proceso[aux+1][3] = True
+                        self.cola_pendiente(self.proceso[aux+1][1],self.proceso[aux+1][2])
+                        self.proceso[aux+1][3] = True   
                     else:
                         if tiempo_proceso == 0:
                             break
                         self.Clasificacion_procesos(tiempo_inicial, self.proceso[aux][1],self.time, True, self.proceso[aux+1][1],tiempo_proceso)
+                        self.cola_pendiente(self.proceso[aux][1],tiempo_proceso)
                         self.proceso[aux][3] = True
                         self.proceso[aux][2] = tiempo_proceso
                         break
@@ -101,18 +112,57 @@ class Interucciones_multiple():
 
     def Clasificacion_procesos(self,tiempo_inicial,dispositivo,tiempo_final, interrumpido, Dispositivo_interrumpio,Tiempo_Termina):
         """Recopila informacion de la simulacion del proceso, para obtener el tiempo inicial, el final, el dispositivo que esta acuando en ese rango de tiemo, y si esyte fue interrumpido o no"""
-        mensaje_interruccion = "No fue interrumpida"
+        mensaje_interrupcion = "No fue interrumpida"
         if interrumpido:
-            mensaje_interruccion = ("Fue interrumpida por el/la {}").format(Dispositivo_interrumpio)
+            mensaje_interrupcion = ("Fue interrumpida por el/la {}").format(Dispositivo_interrumpio)
 
-        self.Clasificacion_procesos_list.append([tiempo_inicial,tiempo_final,dispositivo,mensaje_interruccion, Tiempo_Termina])
+        self.Clasificacion_procesos_list.append([tiempo_inicial,tiempo_final,dispositivo,mensaje_interrupcion, Tiempo_Termina])
 
-    def busqueda_proceso(self,tiempo): 
+    def busqueda_proceso_tiempo(self,tiempo): 
         """Hace busqueda de un proceso mediante el tiempo, retornando el dispositivo que estuvo en ese tiempo, si fue interrumpido, y su rango de tiempo en el proceso"""
         for i in self.Clasificacion_procesos_list:
             if i[0] <= tiempo and tiempo <= i[1]:
                 print(("En el segudo {} se estaba ejecuntado el/la {}\nSu intervalo fue desde el segundo {} al segundo {}\n{}\ntiempo para que finalizara {}seg").format(tiempo,i[2],i[0],i[1],i[3],i[4]))
                 break
+    
+    def busqueda_proceso_nombre(self,nombre):
+        """Hace una busqueda de los procesos en el que estuvo un dispositivo"""
+        interrupcion_total = 0
+        for i in self.Clasificacion_procesos_list:
+            if i[2] == nombre:
+                print("\033[34m")
+                print("____________________________________________________________________________")
+                print(("Estuvo en el rango de {}seg a {}seg\n{}\ntiempo para que finalizara:{}seg").format(i[0],i[1],i[3],i[4]))
+                if not i[3] == "No fue interrumpida":
+                    interrupcion_total = interrupcion_total + 1
+                print("\033[0m")
+        if interrupcion_total == 0:
+            print(("\nEl/La {} Nunca fue interrumpido").format(nombre,interrupcion_total))   
+        elif interrupcion_total == 1:
+            print(("\nEl/La {} fue interrumpido 1 sola vez").format(nombre,interrupcion_total))
+        else:
+            print(("\nEl/La {} fue interrumpida:{} veces").format(nombre,interrupcion_total))            
+
+    def tabla_interrupcion(self):
+        z = "-"
+        print(" _______________________________________")
+        print(("|{:^15}|{:^5}|{:^5}|{:^5}|{:^5}|").format("Dispositivo","Tp1","Tp2","Tp3","Tp4"))
+        print(" _______________________________________")
+        for key in self.Dispositivo_Prioridad:
+            conteiner = []
+            for i in self.cola:
+                if key == i[0]:
+                        conteiner.append(i[1])
+            if len(conteiner) < 4:
+                faltante = 4 - len(conteiner)
+                for j in range(faltante):
+                    conteiner.append(z)
+            print(("""|{:^15}|{:^5}|{:^5}|{:^5}|{:^5}|""").format(key,conteiner[0],conteiner[1],conteiner[2],conteiner[3]))
+            print(" _______________________________________")
+
+
+    
+
                     
                             
            
--- Main.py
+++ Main.py
@@ -3,58 +3,91 @@ import os
 from colorama import *
 
 def main():
-    print("\033[4m"+"Bienvenido al programa de simulacion de interruccion multiple"+"\n\033[0;0m")
-    print("para comenzar introduzca el nombre del dispositivo y seguido la prioridad (ejemp: 'Relog 3) y precione 0 si desea parar de ingresar."+"\n\033[4;31m"+"IMPORTANTE: mientras menos sea el numero de la prioridad, mayor es su prioridad"+"\033[0m")
-    nuevo_usuario = Interucciones_multiple()
-    while True:
-        Dispositivo_Prioridad = input("Introduzca el dispositivo seguido de su prioridad: ")
-        if Dispositivo_Prioridad == "0":
-            break
-        Dispositivo_Prioridad = Dispositivo_Prioridad.split(" ")
-        if  (nuevo_usuario.Agregar_Dispositvo(Dispositivo_Prioridad[0], Dispositivo_Prioridad[1])):
-            print(("\033[;32m"+"Se ha ingresado con exito el disositivo: {} con prioridad {}"+"\033[0m").format(Dispositivo_Prioridad[0],Dispositivo_Prioridad[1]))
-        else:
-            print("\n\033[4;31m"+"Se a obtenido un error con los datos introducido, porfavor introduce nuevamente"+"\033[0m")
-    nuevo_usuario.proceso[0][2] = int(input("\nIntroduzca el tiempo que va a durar el programa sin interruccion: "))
-    print("A continuacion va a desglozar los procesos en el orden que ocurre, primero ingresando el segundo cuando inicia, el dispositvo y la Duracion (ejemplo: 3 computadora 5) en este ejemplo la computadora inica a los 3 seg y su duracion es de 5seg\nPrecione 0 si desea dejar de ingresar proceso")
-    while True:
-        proceso = input("\nIntroduzca el proceso: ")
-        if proceso == "0":
-            break
-        proceso = proceso.split(" ")
-        if  (nuevo_usuario.Agrergar_proceso(proceso[0], proceso[1],proceso[2])):
-            print("\033[3;32m"+"Se ha ingresado con exito el proceso"+"\033[0m")
-        else:
-            print("\n\033[4;31m"+"Se a obtenido un error con los datos introducido, porfavor introduce nuevamente"+"\n\033[0m")
-    tiempo_proceso = nuevo_usuario.simulacion_proceso()
-    print("\033[3;32m"+"\n-----------La simulacion a sido un exito------------------\n"+"\033[0m")
-    print("\033[;;44m")
-    while True:
-        menu_index = int(input(
-        """
+    c = 0
+    while c == 0:
+        print("\033[4m"+"Bienvenido al programa de simulacion de interruccion multiple"+"\n\033[0;0m")
+        print("para comenzar introduzca el nombre del dispositivo y seguido la prioridad"+"\033[36m"+" (ejemp: 'Relog 3) "+"\033[0m"+"y precione 0 si desea parar de ingresar."+"\n\033[4;31m"+"IMPORTANTE: mientras menos sea el numero de la prioridad, mayor es su prioridad"+"\033[0m")
+        nuevo_usuario = Interucciones_multiple()
+        while True:
+            Dispositivo_Prioridad = input("Introduzca el dispositivo seguido de su prioridad: ")
+            if Dispositivo_Prioridad == "0" or Dispositivo_Prioridad == "":
+                break
+            Dispositivo_Prioridad = Dispositivo_Prioridad.split(" ")
+            Dispositivo_Prioridad[0] = Dispositivo_Prioridad[0].lower()
+            if nuevo_usuario.Dispositivo_igual(Dispositivo_Prioridad[0]):
+                print("\033[4;31m"+"Error a ingresado el mismo dispositivo"+"\033[0m")
+            else:
+                if  (nuevo_usuario.Agregar_Dispositvo(Dispositivo_Prioridad[0], Dispositivo_Prioridad[1])):
+                    print(("\033[;32m"+"Se ha ingresado con exito el disositivo: {} con prioridad {}"+"\033[0m").format(Dispositivo_Prioridad[0],Dispositivo_Prioridad[1]))
+                else:
+                    print("\n\033[4;31m"+"Se a obtenido un error con los datos introducido, porfavor introduce nuevamente"+"\033[0m")
+        while True:
+            try:
+                tiempo_programa = int(input("\nIntroduzca el tiempo que va a durar el programa sin interruccion: "))
+                break 
+            except:
+                print("\033[4;31m"+"ERROR: Debe ser un tiempo valido"+"\033[0m") 
+                print("Intentelo nuevamente")
+        nuevo_usuario.proceso[0][2] = tiempo_programa
+        print(("\033[;32m"+"Se agrego con exito el tiempo de duracion del programa:{}"+"\033[0m").format(tiempo_programa))
+        print("\nA continuacion va a desglozar los procesos en el orden que ocurre, primero ingresando el segundo cuando inicia, el dispositvo y la Duracion\n"+"\033[36m"+"(ejemplo: 3 computadora 5) en este ejemplo la computadora inica a los 3 seg y su duracion es de 5seg"+"\033[0m"+"\nPrecione 0 si desea dejar de ingresar proceso")
+        while True:
+            proceso = input("\nIntroduzca el proceso: ")
+            if proceso == "0" or proceso == "":
+                break
+            proceso = proceso.split(" ")
+            proceso[1] = proceso[1].lower()
+            if nuevo_usuario.Dispositivo_igual(proceso[1]):
+                if  (nuevo_usuario.Agrergar_proceso(proceso[0], proceso[1],proceso[2])):
+                    print("\033[3;32m"+"Se ha ingresado con exito el proceso"+"\033[0m")
+                else:
+                    print("\n\033[4;31m"+"Se a obtenido un error con los datos introducido, porfavor introduce nuevamente"+"\n\033[0m")
+            else:
+                print("\n\033[4;31m"+"Error el dispositivo que ha ingresado en el proceso no se encuentra en la base de datos"+"\n\033[0m")
+                print("Los dispositivos en la base de datos son: ")
+                for i in nuevo_usuario.Dispositivo_Prioridad:
+                    if not i == "programa":
+                        print("\033[34m"+i+"\033[0m")
+        tiempo_proceso = nuevo_usuario.simulacion_proceso()
+        print("\033[3;32m"+"\n-----------La simulacion a sido un exito------------------\n"+"\033[0m")
+        
+        while True:
+            menu_index = int(input(
+            """
 
-                _Menu para analizar todo el proceso_
-        ____________________________________________________
-        |0.Para salir                                       |
-        |1.Para hacer una busqueda de tiempo en el proceso  |
-        |2.Para saber cuanto duro el programa               |
-        |___________________________________________________|
+                    _Menu para analizar todo el proceso_
+            ____________________________________________________________
+            |0.Para salir                                               |
+            |1.Para hacer una busqueda de tiempo en el proceso          |
+            |2.Para saber cuanto duro el programa                       |
+            |3.Para hacer busqueda de procesos mediante el Dispositivo  |
+            |4.Ver cola de procesos pendientes                          |
+            |___________________________________________________________|
 
 
 
 """))
-        os.system('cls')
-        if menu_index == 2:
-            print(("\033[4m"+"El tiempo total del programa fue de: {}seg\n").format(nuevo_usuario.time
-            )+"\033[;;44m")
-        elif menu_index == 1:
-            while True:
-                tiempo = int(input("Introduzca en que segundo desea hacer la busqueda: "))
-                nuevo_usuario.busqueda_proceso(tiempo)
-                aux_menu_1 = int(input("\nDesea seguir haciendo busqueda? (para seguir precione:1, de lo contrario:0): "))
-                if aux_menu_1 == 0:
-                    break
-        elif menu_index == 0:
-            break
-    print("\033[0;0m")
+            os.system('cls')
+            if menu_index == 4:
+                nuevo_usuario.tabla_interrupcion()
+            elif menu_index == 3:
+                nombre = input("Ingrese el nombre del disposito: ")
+                nuevo_usuario.busqueda_proceso_nombre(nombre)
+            elif menu_index == 2:
+                print(("El tiempo total del programa fue de: {}seg\n").format(nuevo_usuario.time
+                ))
+            elif menu_index == 1:
+                while True:
+                    tiempo = int(input("Introduzca en que segundo desea hacer la busqueda: "))
+                    nuevo_usuario.busqueda_proceso_tiempo(tiempo)
+                    aux_menu_1 = int(input("\nDesea seguir haciendo busqueda? (para seguir precione:1, de lo contrario:0): "))
+                    if aux_menu_1 == 0:
+                        break
+            elif menu_index == 0:
+                print("\033[3;32m"+"\n----------------Simulacion terminada----------------\n"+"\033[0m")
+                c = int(input(("Desea hacer otra simulacion? (Si:preciones '0', No: precione '1') \n")))
+                break
+            else:
+                print("Ingrese una opcion correcta")
+
 main()
